de7e42c1962431445fc927b02f3a2ada50c305b1
user <ephrahim.lenaiayasa@student.moringaschool.com>
2024-03-12 13:19:17 +0300

    About

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import HomeComponent from "./Home2";
 import Ndoto from "./Ndoto"; // Import the Ndoto component
 import Blog from "./Blog"; // Import the Blog component
+import Home3 from "./Home3";
 
 const Home = () => {
   return (
     <div className="diva">
-     <HomeComponent/>
+     {/* <HomeComponent/> */}
+     <Home3></Home3>
       <Blog />
       <Ndoto />
     </div>
---
+++ src/Home3.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Home3.css"
+
+const Home3 = () => {
+    return(
+        <div className="main-home-3">
+            <div className="home-3-background-image">
+             {/* <h1 className="home-3-header">Welcome to ndoto</h1> */}
+            </div>
+        </div>
+    )
+}
+
+
+export default Home3;
